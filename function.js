190be676e006ee94daf48126a26751c62715a141
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-10-14 19:11:50 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,12 @@
 
 window.function = function (add1,add2,city,state,zip) {
   
+  add1 = add1.value ?? "";
+  add2 = add2.value ?? "";
+  city = city.value ?? "";
+  state = state.value ?? "";
+  zip = zip.value ?? "";
+  
   let sz = [state,zip].filter(Boolean).join(" ");
   
   let res = [add1,add2,city,sz].filter(Boolean).join(", ");
