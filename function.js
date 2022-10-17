5fbd4cd65463937a1b7c5f0f74db84b7a187df78
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2022-10-17 19:06:40 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,11 +1,17 @@
 
 window.function = function (add1,add2,city,state,zip) {
   
-  let szarr = [state,zip];
+  add1 = add1.value ?? "";
+  add2 = add2.value ?? "";
+  city = city.value ?? "";
+  state = state.value ?? "";
+  zip = zip.value ?? "";
+  
+  let szarr = new Array(state,zip);
   
   let sz = szarr.filter(Boolean).join(" ");
   
-  let resarr = [add1,add2,city,sz];
+  let resarr = new Array(add1,add2,city,sz);
   
   let res = resarr.filter(Boolean).join(", ");
   
